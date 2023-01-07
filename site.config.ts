d21ad211e5d4b5653ef4d85c55738d48c7fea1c6
jtmucinor <98220945+jtmucinor@users.noreply.github.com>
2023-01-07 02:47:26 -0600

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,14 +2,14 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'f6eb283aefeb49b0acfff80077c5d4d1',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'blog',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
   author: 'Travis Fischer',
 
